2d55ae37c60171e52fafb40c9327cddd9bc3e1e2
daniiltserin <daniilborowkow@yandex.ru>
2016-11-05 05:06:26 +0600

    New init

--- index.php
+++ index.php
@@ -6,50 +6,13 @@
  * Time: 17:02
  */
 require_once 'vendor/autoload.php';
-require_once 'stopwatch.php';
 
-// connect to database
-$mysqli = new mysqli('db4free.net', 'daniiltserin', '41024910d', 'daniiltserin_bot');
-if (!empty($mysqli->connect_errno)) {
-    echo 'no connection to database';
-    throw new \Exception($mysqli->connect_error, $mysqli->connect_errno);
-}
+$bot = new TelegramBot\Api\Client('279202606:AAEmlhaQxbyKjRWUzUgCUseBKIn1RAkysC4');
 
-// create a bot
-$bot = new \TelegramBot\Api\Client('279202606:AAEmlhaQxbyKjRWUzUgCUseBKIn1RAkysC4');
-// run, bot, run!
+//run bot
 $bot->run();
 
 $bot->command('start', function ($message) use ($bot) {
-    $answer = 'Howdy! Welcome to the stopwatch. Use bot commands or keyboard to control your time.';
+    $answer = 'Hello!';
     $bot->sendMessage($message->getChat()->getId(), $answer);
 });
-
-$bot->command('go', function ($message) use ($bot, $mysqli) {
-    $stopwatch = new Stopwatch($mysqli, $message->getChat()->getId());
-    $stopwatch->start();
-    $bot->sendMessage($message->getChat()->getId(), 'Stopwatch started. Go!');
-});
-
-$bot->command('status', function ($message) use ($bot, $mysqli) {
-    $stopwatch = new Stopwatch($mysqli, $message->getChat()->getId());
-    $answer = $stopwatch->status();
-    if (empty($answer)) {
-        $answer = 'Timer is not started.';
-    }
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-$bot->command('stop', function ($message) use ($bot, $mysqli) {
-    $stopwatch = new Stopwatch($mysqli, $message->getChat()->getId());
-    $answer = $stopwatch->status();
-    if (!empty($answer)) {
-        $answer = 'Your time is ' . $answer . PHP_EOL;
-    }
-    $stopwatch->stop();
-    $bot->sendMessage($message->getChat()->getId(), $answer . 'Stopwatch stopped. Enjoy your time!');
-});
-
-$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/go', '/status']], null, true);
-
-$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboards);
